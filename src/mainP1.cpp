ce92c7b6dca94d9b37bbde756d7b1e8de6372369
KarenJoachin <89163004+KarenJoachin@users.noreply.github.com>
2021-09-01 14:02:12 -0600

    si sale
    :+1:

--- src/mainP1.cpp
+++ src/mainP1.cpp
@@ -11,10 +11,8 @@
 //-----------------------------------------------------------------------------
 //Definiciond de etiquetas
 //-----------------------------------------------------------------------------
+//primera parte
 #define sensor 32
-<<<<<<< Updated upstream
-#define btn1 25
-=======
 #define btn1 12
 //segunda parte
 #define PWMcanalL1 1 // tenemos 16 canales del 0 a 15
@@ -30,7 +28,6 @@
 
 #define PWMcanalS 4    // tenemos 16 canales del 0 a 15
 #define pinPWMServo 21 //para la señal pwm al servo
->>>>>>> Stashed changes
 
 //display7seg
 #define A 15
@@ -49,27 +46,40 @@
 //Prototipo de funciones
 //-----------------------------------------------------------------------------
 void IRAM_ATTR tempB1(void);
-void funcTemp(void);
+//void IRAM_ATTR ledtemp(void);
+
+void configurarPWMLED(void);
+void configurarLED(void);
+
+void confignumeros(void);// para configurar numeros que van a cada display
 
 //-----------------------------------------------------------------------------
 //Variabls Globales
 //-----------------------------------------------------------------------------
-float vaSensor = 0; // variable para el sensor
-float tempC = 0;
+//sensor temp
+float vaSensor = 0.0; //para leer lo que da el sensor
+float tempC = 0.0;    //para temperatura final
+//leds
+int dutycycleLED = 100;
+int dutycycleLED2 = 0;
+
+//servo
+int dutycycle = 6; // es mi angulo 0 del servo
+int temperatura = 0;
+int decenas = 0;
+int unidades = 0;
+int decimal = 0;
 
 //-----------------------------------------------------------------------------
 //ISR
 //-----------------------------------------------------------------------------
-void IRAM_ATTR tempB1()
+void IRAM_ATTR tempB1() //para imprimir valores solo cuando querramos
 {
-<<<<<<< Updated upstream
-=======
   //lectura de sensor en loop para que continuamente este leyendo sin imprimir
   vaSensor = analogRead(sensor);           //leer el valor analógico del sensor
   tempC = vaSensor * (5.0 / 4095.0) * 100; //convertir a Centigrados
   delay(100);                              //espera
 
->>>>>>> Stashed changes
   Serial.print("Temperatura = ");
   Serial.print(tempC, 1);
   Serial.print(" °C  ");
@@ -84,8 +94,6 @@ void setup()
   Serial.begin(115200);
   pinMode(btn1, INPUT_PULLUP);
   attachInterrupt(btn1, tempB1, HIGH);
-<<<<<<< Updated upstream
-=======
   //attachInterrupt(sensor, ledtemp, CHANGE);
   configurarPWMLED();
   configurarLED();
@@ -100,7 +108,6 @@ void setup()
   digitalWrite(display1, LOW);
   digitalWrite(display2, LOW);
   digitalWrite(display3, LOW);
->>>>>>> Stashed changes
 }
 
 //-----------------------------------------------------------------------------
@@ -108,7 +115,100 @@ void setup()
 //-----------------------------------------------------------------------------
 void loop()
 {
-  vaSensor = analogRead(sensor);       //leer el valor analógico del sensor
-  tempC = (vaSensor * 110.0) / 1024.0; //convertir a volts
-  delay(100);                          //esperar 100ms
+  // para leds
+  configurarLED();
+  confignumeros();
+
+//primer display
+  digitalWrite(display1,HIGH);
+  digitalWrite(display2,LOW);
+  digitalWrite(display3,LOW);
+  desplegar7Seg(decenas);
+  desplegarPunto(0);
+  delay(20);
+
+//segundo display
+  digitalWrite(display1,LOW);
+  digitalWrite(display2,HIGH);
+  digitalWrite(display3,LOW);
+  desplegar7Seg(unidades);
+  desplegarPunto(1);
+  delay(20);
+
+//tercer display
+  digitalWrite(display1,LOW);
+  digitalWrite(display2,LOW);
+  digitalWrite(display3,HIGH);
+  desplegar7Seg(decimal);
+  desplegarPunto(0);
+  delay(20);
+}
+
+//-----------------------------------------------------------------------------
+//Funcion para configurar pwm de leds
+//-----------------------------------------------------------------------------
+void configurarPWMLED(void)
+{
+  // primero se configura el modulo pwm
+  ledcSetup(PWMcanalL1, frecPWM, resolucion);
+  ledcSetup(PWMcanalL2, frecPWM, resolucion);
+  ledcSetup(PWMcanalL3, frecPWM, resolucion);
+  //Para leds. Se conecta el GPIO al canal
+  ledcAttachPin(pinPWMR, PWMcanalL1);
+  ledcAttachPin(pinPWMV, PWMcanalL3);
+  ledcAttachPin(pinPWMA, PWMcanalL2);
+}
+
+//-----------------------------------------------------------------------------
+//Funcion para configurar leds y motor
+//-----------------------------------------------------------------------------
+void configurarLED(void)
+{
+  if (tempC <= 19.5) //led verde
+  {
+    ledcWrite(PWMcanalL3, dutycycleLED);
+    ledcWrite(PWMcanalL1, dutycycleLED2);
+    ledcWrite(PWMcanalL2, dutycycleLED2);
+    dutycycle = 6;
+    ledcWrite(PWMcanalS, dutycycle);
+  }
+  if (tempC < 22.5 && tempC > 19.5) //led amarilla
+  {
+
+    ledcWrite(PWMcanalL2, dutycycleLED);
+    ledcWrite(PWMcanalL3, dutycycleLED2);
+    ledcWrite(PWMcanalL1, dutycycleLED2);
+    dutycycle = 13;
+    ledcWrite(PWMcanalS, dutycycle);
+  }
+  if (tempC >= 22.5) //led roja
+  {
+    ledcWrite(PWMcanalL1, dutycycleLED);
+    ledcWrite(PWMcanalL3, dutycycleLED2);
+    ledcWrite(PWMcanalL2, dutycycleLED2);
+    dutycycle = 28;
+    ledcWrite(PWMcanalS, dutycycle);
+  }
+}
+
+//-----------------------------------------------------------------------------
+//Funcion para configurar pwm del servo
+//-----------------------------------------------------------------------------
+void configurarPWMServo(void)
+{ // esto lo puede haber puesto en el setup pero lo prefiero mas sordenado
+  //Paso 1 configurar el modulo PWM
+  ledcSetup(PWMcanalS, frecPWM, resolucion); // esta es el del servo faltan las de las leds
+  //paso 2 seleccionar en que GPIO tendremos nuestras se;al de PWM
+  ledcAttachPin(pinPWMServo, PWMcanalS); //para cambir ciclo de trabajo del servo faltan las de las leds
+}
+
+//-----------------------------------------------------------------------------
+//Funcion para configurar lectura de sensor en deceneas, unidades, decimal
+//-----------------------------------------------------------------------------
+void confignumeros(void)
+{
+  temperatura = tempC * 10; 
+  decenas = temperatura / 100; 
+  unidades = (temperatura - (decenas * 100))/10;
+  decimal = (temperatura-(unidades*10));
 }
